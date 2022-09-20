7e6385058f9dba9660e465fa77cc826a59dd2f23
Lakata <lakataiyela@yahoo.com>
2022-09-20 09:55:06 +0100

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+// const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.get("/cool", (req, res) => res.send(cool()));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+//LEC: use 'new mongoose' when you'll do something fancy with the schema like adding a plugin
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (!err) res.render("secrets");
+    else console.log(err);
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.listen(port, () => console.log("Server is running on port 3000"));
